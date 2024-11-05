973b7f5ed2e616338abfa1aa2dc75d6608258260
Jasper Tan <50438928+GitJasperTan@users.noreply.github.com>
2024-11-05 08:27:47 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jasper Tan on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
