cad4de68becd351c5ea197357e38b5d3dfa2c405
Jasper Tan <50438928+GitJasperTan@users.noreply.github.com>
2024-11-07 08:22:00 -0800

    complete challenge functionality

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 20.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 18
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    var totalPerPerson: Double {
+        return ((checkAmount * (1 + Double(tipPercentage) / 100)) / Double(numberOfPeople + 2))
+    }
+    
+    var totalBillWithTip: Double {
+        return (checkAmount * (1 + Double(tipPercentage) / 100))
+    }
+    
+    let tipPercentages = [0, 15, 18, 20, 25]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            Form {
+                Section("Bill Total") {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) { num in
+                            Text("\(num) people")
+                        }
+                    }
+                    .pickerStyle(.automatic)
+                }
+                
+                // Segmented tip
+//                Section("Tip Percentage") {
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(tipPercentages, id: \.self) { tip in
+//                            Text(tip, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.segmented)
+//                }
+                
+                Section("Tip Percentage") {
+                    Picker("Tip percent", selection: $tipPercentage) {
+                        ForEach(0..<101) { tipPercent in
+                            Text("\(tipPercent) percent")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Bill total + tip") {
+                    Text(totalBillWithTip, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
